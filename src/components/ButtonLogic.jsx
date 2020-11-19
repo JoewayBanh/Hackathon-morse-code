18a423532b583bf371cbb1f03c0a360e3cdd16a7
Joeway <joewaybanh@hotmail.com>
2020-11-19 16:47:02 +0100

    added alphabetical buttons

---
+++ src/components/ButtonLogic.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const ButtonLogic = (props) => {
+  const [alphaLetter, setAlphaLetter] = useState();
+  const [morseLetter, setMorseLetter] = useState();
+
+  const logBtns = (clickedButtonValue) => {
+    setAlphaLetter(props.val[0])
+    setMorseLetter(props.val[1])
+
+    props.handlePassInfoToParent(clickedButtonValue.concat(props.search))
+    console.log(`This is alpha: ${props.val[0]}`);
+    console.log(`This is morse: ${props.val[1]}`);
+  }
+
+  return (
+    <span>
+      <button onClick={() => logBtns(props.val)} >
+        {props.val}
+      </button>
+    </span>
+  )
+}
+
+export default ButtonLogic
